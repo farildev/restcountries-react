b415299c401bca56dd0f96c029b5cdae6447119b
farildev <memmedovfaril@gmail.com>
2023-04-14 23:42:28 +0400

    UPDATE

--- src/App.js
+++ src/App.js
@@ -15,23 +15,27 @@ function App() {
       setData(data)
       setLoading(false)
     })
-    console.log(data);
   },)
   
   return (
     <>
-      <header className="bg-primary p-4 w-100 shadow position-sticky top-0 left-0">
+      <header className="bg-primary d-flex justify-space-between align-items-center p-4 w-100 shadow position-sticky top-0 left-0">
         <div className=" container text-white fs-2">
-          <img src={""} alt={""}/>
+          <h2>Countries</h2>
+        </div>
+        <div className="w-50">
+          <input type="text" className="form-control" placeholder="Enter country name" />
         </div>
       </header>
-      <div className="mt-5 container d-flex justify-content-between align-items-center flex-wrap">
+      <div className="mt-5 container d-flex justify-content-between align-items-center w-full flex-wrap">
       {
         data.map((index, key) => (
-          <div key={key} className="card w-25 p-4">
+          <div key={key} className="card p-3">
+            <div className="card__img">
             <img src={index.flags.svg} alt={index.name.common} />
+            </div>
             <div className="card-body d-flex flex-column py-4">
-              <h5 className="card-title">{index.name.common}</h5>
+              <h5 className="card-title font-weight-bold">{index.name.common}</h5>
               <span className="card-text">Capital : {index.capital}</span>
               <span className="card-text mt-2">Region : {index.region}</span>
               <span className="card-text mt-2">Population : {index.population}</span>
