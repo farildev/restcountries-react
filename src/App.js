f816cf8ca3a3175841403d851fcd526aafc6d358
farildev <memmedovfaril@gmail.com>
2023-04-14 20:41:36 +0400

    UPDATE

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useEffect , useState} from "react";
 import LoadingScreen from "./components/LoadingScreen";
+import "./assets/styles/style.css"
 
 function App() {
 
@@ -14,21 +15,27 @@ function App() {
       setLoading(false)
     })
     console.log(data);
-  }, [])
+  },)
   
   return (
     <>
       <header className="bg-primary p-4 w-100 shadow position-sticky top-0 left-0">
         <div className=" container text-white fs-2">Hello</div>
       </header>
-      <div className="mt-5 container">
-      <div className="card">
-        <img className="card-img-top" src=""></img>
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
+      <div className="mt-5 container d-flex justify-content-between align-items-center flex-wrap">
+      {
+        data.map((index, key) => (
+          <div key={key} className="card w-25 p-4">
+            <img src={index.flags.svg} alt={index.name.common} />
+            <div className="card-body d-flex flex-column py-4">
+              <h5 className="card-title">{index.name.common}</h5>
+              <span className="card-text">{index.capital}</span>
+              <span className="card-text mt-3">{index.region}</span>
+              <span className="card-text mt-3"></span>
+            </div>
+          </div>
+        ))
+      }
       </div>
 
       {
