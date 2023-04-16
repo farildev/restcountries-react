d66237c4870bf93e24a4dfb8a90e77be8d48044c
farildev <memmedovfaril@gmail.com>
2023-04-16 12:36:12 +0400

    UPDATE

--- src/App.js
+++ src/App.js
@@ -1,47 +1,37 @@
 import React, {useEffect , useState} from "react";
 import LoadingScreen from "./components/LoadingScreen";
 import "./assets/styles/style.css"
-import logo from "./assets/images/Countries logo.svg"
 
 function App() {
-
-  const [data , setData] = useState([]);
+  const [region , setRegion] = useState([]); 
+  const [countries , setCountries] = useState([]);
   const [loading , setLoading] = useState(true);
-  const [value , setValue] = useState({});
 
-  const handleInput = (e) => {
-    const inputValue = e.target.value
-    setValue({...value , [e.target.name] : inputValue})
-    console.log(value);
-  }
  
   useEffect(() => {
     fetch("https:restcountries.com/v3.1/all")
     .then(res => res.json())
     .then(data => {
-      setData(data)
+      setCountries(data)
       setLoading(false)
     })
-  },)
-  
+  },[])
+
   return (
     <>
       <header className="bg-primary d-flex justify-space-between align-items-center p-4 w-100 shadow position-sticky top-0 left-0">
         <div className=" container text-white fs-2">
           <h2>Countries</h2>
         </div>
-        <div className="w-50">
-          <input onChange={handleInput} type="text" className="form-control" placeholder="Enter country name" />
-        </div>
       </header>
       <div className="mt-5 container d-flex justify-content-between align-items-center w-full flex-wrap">
       {
-        data.map((index, key) => (
-          <div key={key} className="card p-3 shadow-sm">
+        countries.map((index, key) => (
+          <div key={key} className="card p-3 shadow-sm col-xl-3 col-lg-3 col-md-6 col-sm-12 col-xs-12 mt-4">
             <div className="card__img">
-            <img src={index.flags.svg} alt={index.name.common} />
+            <img src={index.flags.png} alt={index.name.common} />
             </div>
-            <div className="card-body d-flex flex-column py-4">
+            <div className="card-body d-flex flex-column py-4 mt-5">
               <h5 className="card-title font-weight-bold">{index.name.common}</h5>
               <span className="card-text">Capital : {index.capital}</span>
               <span className="card-text mt-2">Region : {index.region}</span>
